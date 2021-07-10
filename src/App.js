f6cf614067bcd30a3ad387527ae5cb7d6aec0e7a
oven425 <oven425@hotmail.com>
2021-07-10 17:00:16 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
 /* eslint-disable no-unused-vars */
 import logo from './logo.svg';
 import './App.css';
-import {Calendar} from './Calendar'
+import { Calendar } from './Calendar'
+import { useState } from 'react';
 
 function App() {
-  const greeting = 'Hello Function Component!';
-  function onSelectCahnge(date){
+  let greeting = 'Hello Function Component!';
+  const onSelectChange1 = (date)=>{
+    console.log(`App onSelectChange`)
     console.log(date)
   }
+  const handleChange = x => {
+    console.log('Headline handleChange')
+
+    console.log(x.target.value)
+  }
+  const handleChange1 = x => {
+    console.log('Headline handleChange1')
+
+    console.log(x)
+  }
   return (
-    <Calendar onSelectCahnge={(date)=>onSelectCahnge(date)}/>
-
+    <div>
+      <Calendar onSelectChange={onSelectChange1}/>
+      <Headline headline={greeting} onChangeHeadline={handleChange} />
+      <Headline1 onChangeHeadline={handleChange1} />
+    </div>
+    
   );
-
-
 }
 
- 
+export const Headline = ({ headline, onChangeHeadline }) => (
+  <div>
+    <h1>{headline}</h1>
+
+    <input type="text" value={headline} onChange={onChangeHeadline} />
+  </div>
+);
+
+export const Headline1= ({ onChangeHeadline }) => (
+  <div onClick={onChangeHeadline}>
+    12345
+  </div>
+);
+
+
+
+
+
 
 export default App;
--- src/Calendar.js
+++ src/Calendar.js
@@ -3,14 +3,12 @@ import { useState, useEffect, useCallback } from "react";
 import PropTypes from 'prop-types';
 import './Calendar.css'
 
-export const Calendar = (onSelectCahnge) => {
+export const Calendar = ({onSelectChange}) => {
 
     const [displayMonth, setDisplayMonth] = useState(new Date())
     const [dates, setDates] = useState(() => {
         let dds = []
-
         dds[0] = new Date();
-
         dds[34] = new Date();
         dds.fill(new Date())
         return dds
@@ -63,41 +61,46 @@ export const Calendar = (onSelectCahnge) => {
     }
 
     function selectDate(x) {
-        console.log('selectDate')
-        console.log(x)
-
+        console.log(`selectDate ${x}`)
+        onSelectChange(x)
+        
     }
 
     return (
         <div className="calendar">
             <div className="calendar_option">
-                <button onClick={prevMonth}>&lt;</button>
-                <div style={{color:"red"}}>{displayMonth.getFullYear()}/{displayMonth.getMonth()}</div>
-                <button onClick={nextMonth} style={{marginLeft:"auto"}}>&gt;</button>
+                <button style={{justifySelf:"start"}} onClick={prevMonth}>&lt;</button>
+                <div style={{justifySelf:"center"}} >{displayMonth.getFullYear()}/{displayMonth.getMonth()}</div>
+                <button style={{justifySelf:"end"}} onClick={nextMonth} >&gt;</button>
             </div>
             <div className="calendaritems_container">
-                <div>日</div>
-                <div>一</div>
-                <div>二</div>
-                <div>三</div>
-                <div>四</div>
-                <div>五</div>
-                <div>六</div>
+                <div style={{justifySelf:"center"}}>日</div>
+                <div style={{justifySelf:"center"}}>一</div>
+                <div style={{justifySelf:"center"}}>二</div>
+                <div style={{justifySelf:"center"}}>三</div>
+                <div style={{justifySelf:"center"}}>四</div>
+                <div style={{justifySelf:"center"}}>五</div>
+                <div style={{justifySelf:"center"}}>六</div>
                 {
                     dates.map((item, index) => {
                         return (
-                            // <div  key={index}>{item.getDate()}</div>
-                            <div onClick={() => selectDate(item)} key={index}>{item.getDate()}</div>
+                             <div style={{justifySelf:"center"}} onClick={()=>{selectDate(item)}} key={index}>{item.getDate()}</div>
+                            //<CalendarItem currentDate={item} key={index}></CalendarItem>
                         )
                     })
                 }
             </div>
+            {/* <input type="text" onChange={onChangeHeadline} /> */}
         </div>
     );
 }
 
-// Calendar.propTypes = {
-//     currentTime: PropTypes.string.isRequired,
-// };
+const CalendarItem=({currentDate})=>{
+    return(
+        <div style={{justifySelf:"center"}}>
+            {currentDate.getDate()}
+        </div>
+    )
+}
 
 
