2c1ce380365366cb361b09501bccf81b5ea90cd9
oven425 <oven425@hotmail.com>
2021-07-30 01:04:31 +0800

    update

--- src/MediaCapture.js
+++ src/MediaCapture.js
@@ -5,42 +5,80 @@ export const MediaCapture = () => {
     const webcam = useRef()
     const snapshotImage = useRef()
     const canvas1 = useRef();
-    // useEffect(()=>{
-    //     const stream = await navigator.mediaDevices.getUserMedia({
-    //         video: {pan: true, tilt: true, zoom: true},
-    //       });
-    // },[])
+    useEffect(()=>{
+        console.log("useEffect 1");
+        async function open() {
+            console.log("open--")
+            const stream = await navigator.mediaDevices.getUserMedia({
+                audio: false,
+                video: { facingMode: "user" },
+            });
+            webcam.current.srcObject = stream;
+    
+            const [track] = stream.getVideoTracks();
+            console.log(track)
+            //imageCapture = new ImageCapture(track);
+            const capabilities = track.getCapabilities();
+            const settings = track.getSettings();
+            console.log("capabilities")
+            console.log(capabilities)
+            console.log("settings")
+            console.log(settings)
+            //imageCapture = new ImageCapture(track);
+    
+            console.log("open----")
+            getFrameFromVideo(webcam.current, canvas1.current);
+        }
+        console.log("useEffect 2");
+        open();
+        console.log("useEffect 3");
+    },[])
 
-    useEffect(() => {
-        console.log("useEffect open--")
-        //open();
-        console.log(canvas1.getContext);
-        if (canvas1.getContext) { //判斷是否支援
-            var ctx = canvas1.getContext('2d');
-            //宣告ctx渲染方式
+//     useEffect(() => {
+//         console.log("useEffect open--")
+//         open();
+//         if (canvas1.current.getContext) { //判斷是否支援
+//             var ctx = canvas1.current.getContext('2d');
+//             //宣告ctx渲染方式
           
-            ctx.fillStyle = "rgb(200,0,0)"; //正方形的填滿色彩
-  ctx.fillRect (10, 10, 130, 130); //繪製一個填滿色彩的正方形
-  ctx.clearRect(20, 20, 110, 110); //摟空一個正方形區域
+//             ctx.fillStyle = "rgb(200,0,0)"; //正方形的填滿色彩
+//   ctx.fillRect (10, 10, 130, 130); //繪製一個填滿色彩的正方形
+//   ctx.clearRect(20, 20, 110, 110); //摟空一個正方形區域
 
-  ctx.strokeStyle = "rgb(0,200,0)"; //正方形線段的色彩
-  ctx.strokeRect(40, 40, 70, 70); //畫一個正方形線段
+//   ctx.strokeStyle = "rgb(0,200,0)"; //正方形線段的色彩
+//   ctx.strokeRect(40, 40, 70, 70); //畫一個正方形線段
           
-          }else {
-            alert('your browser not support canvas')
-            //如果不支援
-          };
-        console.log("useEffect open 1")
-        return (() => {
-            console.log("useEffect open----")
-        })
-    });
+//           }else {
+//             alert('your browser not support canvas')
+//             //如果不支援
+//           };
+//            getFrameFromVideo(webcam.current, canvas1.current);
+//         console.log("useEffect open 1")
+//         return (() => {
+//             console.log("useEffect open----")
+//         })
+
+//     });
+
+    const getFrameFromVideo = (video, canvas) => {
+        
+        const ctx = canvas.getContext("2d");
+        //console.log(`width:${video.width}  height:${video.height}`);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.save();   //儲存狀態
+        ctx.translate(640, 0);
+        ctx.scale(-1, 1);
+        ctx.drawImage(video, 0, 0, 640, 480);
+        ctx.restore(); //到此才輸出，才不會還沒整體操作完就放出，會造成畫面快速抖動
+        requestAnimationFrame(() => getFrameFromVideo(video, canvas));
+      };
 
     let imageCapture;
     async function open() {
         console.log("open--")
         const stream = await navigator.mediaDevices.getUserMedia({
-            video: { pan: true, tilt: true, zoom: true },
+            audio: false,
+            video: { facingMode: "user" },
         });
         webcam.current.srcObject = stream;
 
@@ -97,17 +135,19 @@ export const MediaCapture = () => {
 
     return (
         <div style={{
-            width: "500px",
+            width: "640px",
             border: "1px #333 solid"
         }}>
             <video ref={webcam} id="video" style={{
-                width: "500px",
-                height: "375px",
+                width: "640px",
+                height: "480px",
                 background: "#666"
             }} autoPlay></video>
-            <canvas width="300" height="300" ref={canvas1}></canvas>
+            <canvas width="640" height="480" ref={canvas1} style={{border:"blue solid 1px"}}>
+            你的浏览器不支持 canvas，请升级你的浏览器。
+            </canvas>
             
-            <label>
+            {/* <label>
                 <input type="radio" value="aaa" name="gender" onClick={()=>photoMode()} />
                 <span>Photo</span>
             </label>
@@ -121,11 +161,10 @@ export const MediaCapture = () => {
                 height: "200px",
                 border: "1px #333 solid"
             }}></img>
-            {/* <input type="checkbox">Record</input> */}
             <label>WB</label>
             <select>
                 <option></option>
-            </select>
+            </select> */}
         </div>
 
     )
