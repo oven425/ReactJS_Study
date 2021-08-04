af3b25824b948bd463d6446c813e98661ac968d8
oven425 <oven425@hotmail.com>
2021-08-05 00:33:26 +0800

    update

--- src/App.js
+++ src/App.js
@@ -5,28 +5,30 @@ import { Calendar } from './Calendar'
 import { MediaCapture } from './MediaCapture';
 import { useState, useMemo } from 'react';
 import { useRef } from 'react';
+import { ImageEdit } from './ImageEdit';
 
 
 function App() {
 
-  const calendar = useRef()
-  const onSelectChange1 = (date) => {
-    console.log(`App onSelectChange`)
-    console.log(date)
-  }
+  // const calendar = useRef()
+  // const onSelectChange1 = (date) => {
+  //   console.log(`App onSelectChange`)
+  //   console.log(date)
+  // }
 
-  const nextMonth = ()=>{
-    console.log("App nextMonth")
-    console.log(calendar)
-    calendar.current.nextMonth()
-  }
-  
+  // const nextMonth = ()=>{
+  //   console.log("App nextMonth")
+  //   console.log(calendar)
+  //   calendar.current.nextMonth()
+  // }
+  //<ImageEdit></ImageEdit>
   return (
-    <div>
-      <MediaCapture></MediaCapture>
-      <button onClick={nextMonth}>Test calendar method</button>
-      <Calendar ref={calendar} onSelectChange={onSelectChange1}/>
-    </div>
+    <ImageEdit></ImageEdit>
+    // <div>
+    //   <MediaCapture></MediaCapture>
+    //   <button onClick={nextMonth}>Test calendar method</button>
+    //   <Calendar ref={calendar} onSelectChange={onSelectChange1}/>
+    // </div>
 
   );
 }
---
+++ src/ImageEdit.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-unused-vars */
+import { red } from "@material-ui/core/colors";
+import { useRef, useEffect } from "react"
+import './ImageEdit.css'
+
+export const ImageEdit = () => {
+    const canvas=useRef();
+
+    useEffect(()=>{
+        fillRect(100,100,100,100);
+    },[]);
+
+    const fillRect = (x, y, width, height) => {
+
+        const ctx = canvas.current.getContext("2d");
+        //console.log(`width:${video.width}  height:${video.height}`);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.save();   //儲存狀態
+        ctx.fillStyle = '#7cce2b';
+        ctx.fillRect(x, y, width, height);
+        ctx.restore(); //到此才輸出，才不會還沒整體操作完就放出，會造成畫面快速抖動
+    };
+
+    
+
+    return (
+        <div className="box">
+            <div className="row header">
+                Riibon
+            </div>
+            <div className="row content">
+                <div style={{marginLeft:"5px", marginTop:"5px", backgroundColor:"red", width:"300px", height:"300px"}}>
+                    <canvas ref={canvas} width="300px" height="300px"></canvas>
+                </div>
+            </div>
+            <div className="row footer">
+                status bar
+            </div>
+        </div>
+    )
+
+}
