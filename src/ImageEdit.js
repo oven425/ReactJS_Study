7c2629124555377629b3db96bf0f2370bb87a449
oven425 <oven425@hotmail.com>
2021-08-16 00:41:24 +0800

    update

--- src/ImageEdit.js
+++ src/ImageEdit.js
@@ -14,18 +14,23 @@ export const ImageEdit = () => {
 
         const ctx = canvas.current.getContext("2d");
         //console.log(`width:${video.width}  height:${video.height}`);
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.clearRect(0, 0, canvas.current.width, canvas.current.height);
         ctx.save();   //儲存狀態
-        ctx.fillStyle = '#7cce2b';
-        ctx.fillRect(x, y, width, height);
+        ctx.fillStyle = '#ffffff';
+        //ctx.fillRect(x, y, width, height);
+        ctx.fillRect(0, 0, canvas.current.width, canvas.current.height);
+        
         ctx.restore(); //到此才輸出，才不會還沒整體操作完就放出，會造成畫面快速抖動
     };
 
+
+
     const canvas_Mousemove = (data)=>{
-        var rect = data.target.getBoundingClientRect();
-        console.log(rect);
+        //var rect = data.target.getBoundingClientRect();
+        //console.log(rect);
         
-        console.log(`x:${data.clientX-rect.x} y:${data.clientY-rect.y}`);
+        //console.log(`x:${data.clientX-rect.x} y:${data.clientY-rect.y}`);
+        //console.log(data);
     }
 
     return (
@@ -34,8 +39,11 @@ export const ImageEdit = () => {
                 Riibon
             </div>
             <div className="row content">
-                <div onMouseMove={(e)=>canvas_Mousemove(e)} style={{marginLeft:"0px", marginTop:"0px", backgroundColor:"red", width:"300px", height:"300px"}}>
-                    <canvas ref={canvas} width="300px" height="300px"></canvas>
+                <div style={{display:"grid", gridColumn:"auto, 20px", marginLeft:"5px", marginTop:"5px", backgroundColor:"blue", width:"320px", height:"320px"}}>
+                    <canvas onMouseMove={(e)=>canvas_Mousemove(e)} ref={canvas} width="300px" height="300px"></canvas>
+                    <div style={{width:"20px", gridColumn:"2", gridRow:"2", height:"20px", background:"red", cursor:"se-resize"}}></div>
+                    <div style={{width:"20px", gridColumn:"1", gridRow:"2", justifySelf:"center", height:"20px", background:"red", cursor:"s-resize"}}></div>
+                    <div style={{width:"20px", gridColumn:"2", gridRow:"1", alignSelf:"center", height:"20px", background:"red", cursor:"w-resize"}}></div>
                 </div>
             </div>
             <div className="row footer">
